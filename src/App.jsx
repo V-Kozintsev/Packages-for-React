2739183dd0785dd6e8d0c8bef706adf3a1731c29
Viktor <v_kozintcev@mail.ru>
2025-01-28 19:14:03 +0300

    подключение reactDOM

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//App.jsx
+import React from "react";
+
+const a = 0;
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>count: {a}</p>
+    </div>
+  );
+}
+
+export default App;
