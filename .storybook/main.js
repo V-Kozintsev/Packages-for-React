dd5fc659ebc7d78579441c8311308c8768b1830f
Viktor <v_kozintcev@mail.ru>
2025-02-01 17:26:23 +0300

    add component Heading and stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 // .storybook/main.js
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-onboarding",
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,3 +5,4 @@ export const Heading = ({ level, className, children }) => {
   const HeadingTag = `h${level}`;
   return <HeadingTag className={className}>{children}</HeadingTag>;
 };
+
